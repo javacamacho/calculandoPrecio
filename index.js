39ba0a350dc1b8584ac8027abc7ba3652609c9e2
Javiera Camacho <javiera.camacho@gmail.com>
2024-06-06 21:55:31 -0400

    subiendo archivos

---
+++ index.js
@@ -0,0 +1,10 @@
+precio = 400000
+
+let cantidad = 0;
+let valorTotal = 0;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+document.querySelector('.cantidad').innerHTML = cantidad;
+document.querySelector('.valor-total').innerHTML = valorTotal;
